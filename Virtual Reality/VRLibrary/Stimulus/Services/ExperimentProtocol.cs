1023b962a1f1ad1d3dfab2f77c08db717cab7f72
Tomás Cruz <tomaslcruz@gmail.com>
2018-11-22 18:43:04 +0000

    Add files via upload

---
+++ Virtual Reality/VRLibrary/Stimulus/Services/ExperimentProtocol.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VRLibrary.Stimulus.Services
+{
+    public class ExperimentProtocol
+    {
+        public float duration;
+        public float durationTrial;
+        public List<string> stimTypes;
+        public ExperimentProtocol(IServiceProvider WObj, float duration)
+        {
+            this.duration = duration;
+        }
+        public ExperimentProtocol(IServiceProvider WObj, float duration, float dTrial, List<string> sTypes)
+        {
+            this.duration = duration;
+            this.durationTrial = dTrial;
+            this.stimTypes = sTypes;
+        }
+    }
+}
