20fc95f5332ed52921eaebd959339af38dd18e2c
Tomás Cruz <tomaslcruz@gmail.com>
2018-11-22 18:43:39 +0000

    Add files via upload

---
+++ Virtual Reality/VRLibrary/Stimulus/ServiceFactories/ExperimentProtocolFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using VRLibrary.Stimulus.Services;
+
+namespace VRLibrary.Stimulus.ServiceFactories
+{
+    public class ExperimentProtocolFactory : ServiceFactory
+    {
+        public float duration;
+        public float durationTrials;
+        public List<string> stimTypes;
+        public override void Initialize(IServiceProvider provider, Game game)
+        {
+            var wo = (IServiceContainer)provider.GetService(typeof(IServiceContainer));
+            var ep = new ExperimentProtocol(provider, duration, durationTrials, stimTypes);
+            wo.AddService(typeof(ExperimentProtocol), ep);
+        }
+    }
+}
